81c4191bbfde3de5bef9afa27ca32fb6040ad964
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-13 22:09:46 -0300

    database schema

---
+++ back/prisma/migrations/000000000000_initial/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "game" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "password" TEXT,
+    "creator_ip" TEXT,
+    "initialAmount" DECIMAL NOT NULL DEFAULT 0,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "player" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL DEFAULT 'Player',
+    "color" TEXT NOT NULL DEFAULT '#ffffff',
+    "game_id" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "player_game_id_fkey" FOREIGN KEY ("game_id") REFERENCES "game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "transaction" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "player_id" INTEGER NOT NULL,
+    "amount" DECIMAL NOT NULL,
+    "operation" INTEGER NOT NULL,
+    "ip" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "transaction_player_id_fkey" FOREIGN KEY ("player_id") REFERENCES "player" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "player_color_game_id_key" ON "player"("color", "game_id");
+
