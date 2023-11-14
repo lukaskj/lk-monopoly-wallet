93346a08ffab00c971073487a723f1c60106b1e8
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-14 18:34:09 -0300

    temp: validation filter

--- back/src/common/filters/validation-error-exception-filter.ts
+++ back/src/common/filters/validation-error-exception-filter.ts
@@ -1,26 +1,70 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus, Logger, ValidationPipe } from "@nestjs/common";
-import { ValidationError } from "class-validator";
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  ValidationError,
+  ValidationPipe,
+} from "@nestjs/common";
 
-@Catch(Array<ValidationError>)
+@Catch()
 export class ValidationErrorExceptionFilter extends ValidationPipe implements ExceptionFilter {
-  private readonly logger: Logger = new Logger(ValidationErrorExceptionFilter.name);
+  // private readonly logger: Logger = new Logger(ValidationErrorExceptionFilter.name);
 
-  catch(exception: Array<ValidationError>, host: ArgumentsHost): void {
+  constructor() {
+    super({
+      transform: true,
+      stopAtFirstError: true,
+    });
+  }
+
+  protected exceptionFactory = (errors: ValidationError[]): any => {
+    const result = errors.map((error) => ({
+      property: error.property,
+      message: error.constraints ? error.constraints[Object.keys(error.constraints)[0]] : "",
+    }));
+
+    return new BadRequestException(result);
+  };
+
+  catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
-    const statusCode = HttpStatus.BAD_REQUEST;
-    const message = this.flattenValidationErrors(exception);
-    const error = "Bad Request";
+    if (exception instanceof HttpException) {
+      const exResponse = exception.getResponse();
+      const error = exception.name;
+      let message = "";
+      if (typeof exResponse === "object" && "message" in exResponse) {
+        message = exResponse.message as string;
+      }
+      const statusCode = exception.getStatus();
+      // const error = exception.message;
+      console.log({
+        statusCode,
+        exception,
+        exResponse,
+        message,
+      });
 
-    const json = {
-      statusCode,
-      message,
-      error,
-    };
+      response.status(statusCode).json({
+        statusCode,
+        message: error,
+      });
+    }
+    // const statusCode = HttpStatus.BAD_REQUEST;
+    // const message = this.flattenValidationErrors(exception);
+    // const error = "Bad Request";
 
-    this.logger.error([json, exception]);
+    // const json = {
+    //   statusCode,
+    //   message,
+    //   error,
+    // };
 
-    response.status(statusCode).json(json);
+    // this.logger.error([json, exception]);
+
+    // response.status(500).json({});
   }
 }
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,17 +1,19 @@
 import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
+import * as dotenv from "dotenv";
 import { AppModule } from "./app.module";
 import "./common/bigint-transform-fix";
-import { ValidationErrorExceptionFilter } from "./common/filters/validation-error-exception-filter";
 import { LoggingInterceptor } from "./common/interceptors/logging-interceptor";
-import * as dotenv from "dotenv";
 
 async function bootstrap(): Promise<void> {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.useGlobalFilters(new ValidationErrorExceptionFilter());
+  // const validationExceptionPipeFilter = new ValidationErrorExceptionFilter();
+  // app.useGlobalPipes(validationExceptionPipeFilter);
+  // app.useGlobalFilters(validationExceptionPipeFilter);
+
   app.useGlobalInterceptors(
     // new HideSensitiveInformationInterceptor(),
     new LoggingInterceptor(),
