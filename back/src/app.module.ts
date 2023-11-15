1864abd245bb211552a3292697b25c47ee03633d
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-15 00:19:50 -0300

    removed unused code

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { WalletModule } from "./wallet/wallet.module";
 
 @Module({
   imports: [WalletModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
