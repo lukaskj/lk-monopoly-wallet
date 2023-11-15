40582f372aff88ba5d5c9e2ce0f7ac6bd01b428b
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-15 02:05:17 -0300

    feat: add new player to game

--- back/src/wallet/interface/controllers/game/game.controller.ts
+++ back/src/wallet/interface/controllers/game/game.controller.ts
@@ -3,28 +3,45 @@ import { SerializeTo } from "@common/decorators/serialize-to";
 import { NotEmptyPipe } from "@common/pipes/not-empty.pipe";
 import { Body, Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
 import { GameService } from "../../../application/services/game/game.service";
+import { PlayerService } from "../../../application/services/player/player.service";
 import { GameViewModel } from "../../viewmodels/game/game.viewmodel";
+import { CreateGamePlayerViewmodel } from "../../viewmodels/game/request/create-game-player.viewmodel";
 import { CreateGameViewmodel } from "../../viewmodels/game/request/create-game.viewmodel";
+import { PlayerViewModel } from "../../viewmodels/game/player.viewmodel";
 
 @Controller("/v1/game")
 export class GameController {
-  constructor(private readonly gameService: GameService) {
+  constructor(
+    private readonly gameService: GameService,
+    private readonly playerService: PlayerService,
+  ) {
     this.gameService;
   }
 
   @Get(":id")
   @SerializeTo(GameViewModel)
-  public async getGame(@Param("id", NotEmptyPipe, ParseIntPipe) id: number): Promise<object> {
+  public async getGame(@Param("id", NotEmptyPipe, ParseIntPipe) id: number) {
     return await this.gameService.getGameById(id);
   }
 
   @Post("")
   @SerializeTo(GameViewModel)
-  public async createGame(@Body() body: CreateGameViewmodel, @RealIP() ip: string): Promise<object> {
+  public async createGame(@Body() body: CreateGameViewmodel, @RealIP() ip: string) {
     const game = await this.gameService.createGame({
       ...body,
       creatorIp: ip,
     });
     return game;
+  }
+
+  @Post(":id/player")
+  @SerializeTo(PlayerViewModel)
+  public async addNewPlayerToGame(@Param("id") id: number, @Body() body: CreateGamePlayerViewmodel) {
+    return await this.playerService.createPlayer(
+      {
+        ...body,
+      },
+      id,
+    );
   }
 }
