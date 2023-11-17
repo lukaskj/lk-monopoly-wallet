50c4e9edff2c880ea3d14f38499c1670fbffa7ac
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-16 23:32:06 -0300

    fix: amount type

--- back/test/fixtures/game.fixture.ts
+++ back/test/fixtures/game.fixture.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { Game, Prisma } from "@prisma/client";
+import { Game } from "@prisma/client";
 import { CreateGameDto } from "../../src/wallet/application/dto/create-game.dto";
 
 export function _fixtureGame(data?: Partial<Game>): Game {
@@ -8,7 +8,7 @@ export function _fixtureGame(data?: Partial<Game>): Game {
     name: faker.person.jobArea(),
     creatorIp: faker.internet.ip(),
     finished: faker.datatype.boolean(),
-    initialAmount: Prisma.Decimal.sum(faker.number.int({ min: 100, max: 100000 })),
+    initialAmount: faker.number.int({ min: 100, max: 100000 }),
     password: faker.string.alphanumeric({ length: 5 }),
     createdAt: faker.date.anytime(),
     updatedAt: faker.date.anytime(),
