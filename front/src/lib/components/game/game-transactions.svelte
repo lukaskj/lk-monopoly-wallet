d19d6db7caf7d33dd584cd921c798769d49c28ec
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-19 01:55:29 -0300

    feat: transaction list

---
+++ front/src/lib/components/game/game-transactions.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Transaction } from "$lib/dto";
+  import { CurrencyFormatter } from "$lib/helpers/currency-formatter";
+
+  export let transactions: Transaction[];
+</script>
+
+<ul class="list p-4">
+  {#each transactions as tr}
+    <li>
+      <span class={`badge  ${tr.operation === 1 ? "bg-success-300 rotate-180" : "bg-error-300"}`}>
+        <i class="icon:fall-down"></i>
+      </span>
+      <span class="flex flex-row w-full justify-between border-2 border-dashed">
+        <span>{tr.player.name}</span>
+        <span class={tr.operation === 1 ? `text-success-500 font-medium` : `text-error-500 font-medium`}>
+          {CurrencyFormatter.format(tr.amount * tr.operation)}
+        </span>
+      </span>
+    </li>
+    <hr />
+  {/each}
+</ul>
--- front/src/routes/game/[id]/+page.svelte
+++ front/src/routes/game/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import GameTransactions from "$lib/components/game/game-transactions.svelte";
   import { layoutTitleStore } from "$lib/stores/layout-title.store";
   import type { PageData } from "./$types";
   import type { TGameData } from "./types";
@@ -6,5 +7,6 @@
   export let data: PageData;
   let { game, players, transactions } = data.gameData as TGameData;
   layoutTitleStore.set(game.name);
-  console.log("AAAAAAAAAAA", players, transactions);
 </script>
+
+<GameTransactions transactions={transactions.data} />
