5b9c654f2b44069eae73f3877022a19012f0c905
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-20 10:04:34 -0300

    build script

--- back/scripts/build-prod.sh
+++ back/scripts/build-prod.sh
@@ -2,7 +2,14 @@
 rm -rf dist
 nest build --webpack --webpackPath ./webpack.config.js
 # nest build
-cp .env pnpm-lock.yaml package.json dist/
+cp pnpm-lock.yaml package.json dist/
+
+[[ -e 'prisma/schema.prisma' ]] && cp -r prisma/ dist/
+[[ -e 'prisma/schema.prisma' ]] && cp prisma/schema.prisma dist/
+[[ -e 'node_modules/prisma/query_engine-windows.dll.node' ]] && cp 'node_modules/prisma/query_engine-windows.dll.node' dist/
+[[ -e 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' ]] && cp 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' dist/
+
+exit 0
 
 # BASH
 # FILES=('prisma/schema.prisma' 'node_modules/prisma/query_engine-windows.dll.node')
@@ -13,6 +20,3 @@ cp .env pnpm-lock.yaml package.json dist/
 #   fi
 # done
 
-[[ -e 'prisma/schema.prisma' ]] && cp 'prisma/schema.prisma' dist/
-[[ -e 'node_modules/prisma/query_engine-windows.dll.node' ]] && cp 'node_modules/prisma/query_engine-windows.dll.node' dist/
-[[ -e 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' ]] && cp 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' dist/
