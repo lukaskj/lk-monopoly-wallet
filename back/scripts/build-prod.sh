14b51ee1e1d0cce772a0e44181e79f09ba0e79eb
Lukas Jacomin <lukasjacomin@gmail.com>
2024-03-30 00:55:34 -0300

    fix: prisma build prod script

--- back/scripts/build-prod.sh
+++ back/scripts/build-prod.sh
@@ -8,6 +8,7 @@ cp pnpm-lock.yaml package.json dist/
 [[ -e 'prisma/schema.prisma' ]] && cp prisma/schema.prisma dist/
 [[ -e 'node_modules/prisma/query_engine-windows.dll.node' ]] && cp 'node_modules/prisma/query_engine-windows.dll.node' dist/
 [[ -e 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' ]] && cp 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' dist/
+[[ -e 'node_modules/prisma/libquery_engine-linux-musl-arm64-openssl-3.0.x.so.node' ]] && cp 'node_modules/prisma/libquery_engine-linux-musl-arm64-openssl-3.0.x.so.node' dist/
 
 exit 0
 
