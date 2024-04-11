31348cb34b78a95501a5921e7854ed94e016f0b4
Lukas Jacomin <lukasjacomin@gmail.com>
2024-04-10 23:31:24 -0300

    fix: deploy

--- docker/deploy-back.sh
+++ docker/deploy-back.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # docker compose up -d --no-deps --build monopoly-wallet-backend
 
-docker compose build
+docker compose build monopoly-wallet-backend
 docker compose down monopoly-wallet-backend
 docker compose up -d monopoly-wallet-backend
--- docker/deploy-front.sh
+++ docker/deploy-front.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker compose build
+docker compose build monopoly-wallet-frontend
 docker compose down monopoly-wallet-frontend
 docker compose up -d monopoly-wallet-frontend
